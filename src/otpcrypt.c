5cddc42ac9eebe7016e5572bf8adfc6f3ebac60e
Alec Hitchiner <kd8rgp@gmail.com>
2017-01-30 09:36:30 -0500

    Made otpcrypt.c pretty, fixed minor memory leak.

--- src/otpcrypt.c
+++ src/otpcrypt.c
@@ -7,11 +7,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define OTP_BLOCK_SIZE 128
+
 int main(int argc, char **argv){
 	char *key_name;
 	char *crypt_name;
 	char *output_name;
 
+	/* Check Args */
 	if(argc < 2 || argc > 3){
 		printf(	"Usage:\n"
 			"Encryption: %s [input file]\n"
@@ -20,7 +23,9 @@ int main(int argc, char **argv){
 		return -1;
 	}
 
+	/* Encryption routine */
 	if(argc == 2){
+		/* Generate filenames for output files */
 		key_name = strdup(argv[1]);
 		crypt_name = realloc(key_name, strlen(key_name) + 5);
 		strcat(key_name, ".key");
@@ -29,22 +34,31 @@ int main(int argc, char **argv){
 		crypt_name = realloc(crypt_name, strlen(crypt_name) + 5);
 		strcat(crypt_name, ".otp");
 
-		if(otp_encrypt_file(argv[1], crypt_name, key_name, 100, 1)){
+		/* Perform encryption */
+		if(otp_encrypt_file(argv[1], crypt_name,
+					key_name, OTP_BLOCK_SIZE, 1)){
 			fprintf(stderr, "Error ocurred during encryption.\n");
 			return -1;
 		}
 
+		/* Free name buffers */
 		free(key_name);
 		free(crypt_name);
 	}
+	/* Decryption routine */
 	else{
+		/* Generate output name */
 		output_name = strdup(argv[1]);
 		output_name[strlen(output_name) - 4] = '\0';
 
+		/* Perform decryption */
 		if(otp_decrypt_file(argv[1], argv[2], output_name, 100)){
 			fprintf(stderr, "Error ocurred during decryption.\n");
 			return -1;
 		}
+
+		/* Free name buffer */
+		free(output_name);
 	}
 		
 	return 0;
